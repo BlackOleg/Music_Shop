f4aea16cff464e94b97360f1dfa02c5d7f1727ba
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-12-27 03:08:43 +0400

    Uploading file

--- src/main/java/musicshop/service/SongService.java
+++ src/main/java/musicshop/service/SongService.java
@@ -3,10 +3,41 @@ package musicshop.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Objects;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SongService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.Objects;
 
+    @Service
+    public class Mp3FileService {
+
+        public void uploadMp3File(MultipartFile file) throws IOException {
+            String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+
+            // Parsing filename to extract name and extension
+            String[] fileNameParts = fileName.split("\\.");
+            String name = fileNameParts[0]; // Extracting name
+            String extension = fileNameParts[1]; // Extracting extension
+
+            // Save file details to the database using JPA repository
+//            Mp3File mp3File = new Mp3File();
+//            mp3File.setName(name);
+//            mp3File.setExtension(extension);
+//            mp3File.setData(file.getBytes()); // Assuming 'data' is a field to store the file content
+//            mp3FileRepository.save(mp3File);
+        }
+    }
 }
+
